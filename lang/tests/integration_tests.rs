ac9a8a43e57d06e4b18295027a89697f04a8e319
cyteon <me@cyteon.dev>
2026-01-24 17:20:20 +0100

    integration testing

---
+++ lang/tests/integration_tests.rs
@@ -0,0 +1,31 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use std::fs;
+use std::path::Path;
+
+fn run_test(name: &str) {
+    let dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests").join("cases");
+    let modu_file = dir.join(format!("{}.modu", name));
+    let expected_file = dir.join(format!("{}.expected", name));
+
+    let expected_output = fs::read_to_string(&expected_file)
+        .expect("Failed to read expected output file");
+
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .expect("Failed to find binary")
+        .arg("run")
+        .arg(modu_file)
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(expected_output));
+}
+
+#[test]
+fn basic_print() {
+    run_test("basic_print");
+}
+
+#[test]
+fn nesting_funcs() {
+    run_test("nesting_funcs");
+}
