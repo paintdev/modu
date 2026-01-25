6e51179ad5ac308fd24528e20c86eb1a213f8a07
cyteon <me@cyteon.dev>
2026-01-25 12:05:00 +0100

    way better ffi

--- lang/examples/ffi/ffi_test/src/lib.rs
+++ lang/examples/ffi/ffi_test/src/lib.rs
@@ -1,109 +1,51 @@
+use modu_ffi::*;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn add(
     argc: std::ffi::c_int,
-    argv: *const *const std::ffi::c_char
-) -> i32 {
+    argv: *const FFIValue
+) -> FFIValue {
     if argc != 2 {
         panic!("add requires 2 arguments");
     }
 
-    let args = unsafe {
-        std::slice::from_raw_parts(argv, argc as usize)
-    };
+    unsafe {
+        let a = (*argv.offset(0 as isize)).value.integer;
+        let b = (*argv.offset(1 as isize)).value.integer;
 
-    // modu ffi cant have numbers as args cause shit broke
-    let num1 = unsafe {
-        std::ffi::CString::from_raw(args[0] as *mut std::ffi::c_char)
-    };
-    let num1 = num1.to_str().unwrap().parse::<i32>().unwrap();
-
-    let num2 = unsafe {
-        std::ffi::CString::from_raw(args[1] as *mut std::ffi::c_char)
-    };
-    let num2 = num2.to_str().unwrap().parse::<i32>().unwrap();
-
-    num1 + num2
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn a(
-    argc: std::ffi::c_int,
-    argv: *const *const std::ffi::c_char
-) -> i32 {
-    if argc != 1 {
-        panic!("a requires 1 argument");
+        FFIValue::integer(a + b)
     }
-
-    let args = unsafe {
-        std::slice::from_raw_parts(argv, argc as usize)
-    };
-
-    let a = unsafe {
-        std::ffi::CString::from_raw(args[0] as *mut std::ffi::c_char)
-    };
-    let a = a.to_str().unwrap();
-
-    a.parse().unwrap()   
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn one() -> i64 {
-    1
+pub extern "C" fn one() -> FFIValue {
+    FFIValue::integer(1)
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn string() -> *mut std::ffi::c_char {
-    std::ffi::CString::new("Hello, World!").unwrap().into_raw()
+pub extern "C" fn string() -> FFIValue {
+    FFIValue::string(std::ffi::CString::new("Hello from Rust!").unwrap().into_raw())
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn print(
     argc: std::ffi::c_int,
-    argv: *const *const std::ffi::c_char
+    argv: *const FFIValue
 ) {
     if argc != 1 {
         panic!("print requires 1 argument");
     }
 
-    let args = unsafe {
-        std::slice::from_raw_parts(argv, argc as usize)
-    };
-
     let str = unsafe {
-        std::ffi::CStr::from_ptr(args[0])
+        std::ffi::CStr::from_ptr((*argv.offset(0 as isize)).value.string)
     };
 
     println!("{}", str.to_str().unwrap());
 }
 
-#[unsafe(no_mangle)]
-pub extern "C" fn print2(
-    argc: std::ffi::c_int,
-    argv: *const *const std::ffi::c_char
-) {
-    if argc != 2 {
-        panic!("print requires 2 arguments");
-    }
-
-    let args = unsafe {
-        std::slice::from_raw_parts(argv, argc as usize)
-    };
-
-    let str = unsafe {
-        std::ffi::CStr::from_ptr(args[0])
-    };
-
-    let str2 = unsafe {
-        std::ffi::CStr::from_ptr(args[1])
-    };
-
-    print!("{}", str.to_str().unwrap());
-    print!("{}", str2.to_str().unwrap());
-    println!();
-}
-
 
 #[unsafe(no_mangle)]
-pub extern "C" fn hello_world() {
+pub extern "C" fn hello_world() -> FFIValue {
     println!("Hello, World!");
+    FFIValue::null()
 }
--- lang/src/packages/ffi.rs
+++ lang/src/packages/ffi.rs
@@ -3,6 +3,10 @@ use std::path::PathBuf;
 use crate::ast::AST;
 use crate::eval::eval;
 
+use modu_ffi::{FFIValue, FFIType};
+
+type FFIFunction = unsafe extern "C" fn(i32, *const FFIValue) -> FFIValue;
+
 pub fn call(mut args: Vec<AST>, context: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
     // (path_to_lib, function_name, arg1, arg2, ...)
 
@@ -34,66 +38,62 @@ pub fn call(mut args: Vec<AST>, context: &mut HashMap<String, AST>) -> Result<(A
 
     let name = match eval(args[1].clone(), context) {
         Ok(AST::String(v)) => v,
-
         _ => return Err("ffi.call second argument must be a string".to_string()),
     };
+
+    let mut ffi_args = Vec::<FFIValue>::new();
+    let mut owned_strings = Vec::<*mut std::ffi::c_char>::new();
+
+    for arg in args.drain(2..) {
+        match eval(arg, context)? {
+            AST::Null => ffi_args.push(FFIValue::null()),
+            AST::String(v) => {
+                let c = std::ffi::CString::new(v).unwrap();
+                let ptr = c.into_raw();
+
+                owned_strings.push(ptr);
+                ffi_args.push(FFIValue::string(ptr));
+            }
+            AST::Integer(v) => ffi_args.push(FFIValue::integer(v)),
+            AST::Float(v) => ffi_args.push(FFIValue::float(v)),
+            AST::Boolean(v) => ffi_args.push(FFIValue::boolean(v)),
+
+            _ => return Err("unsupported FFI argument".into()),
+        }
+    }
 
     unsafe {
         let lib = match libloading::Library::new(path) {
             Ok(lib) => lib,
-            Err(e) => return Err(format!("Failed to load library: {}", e)),
+            Err(e) => return Err(format!("failed to load library: {}", e)),
         };
 
-        let func: libloading::Symbol<unsafe extern "C" fn(
-            argc: std::ffi::c_int,
-            argv: *mut std::ffi::c_char
-        ) -> *mut std::ffi::c_void> 
+        let func: libloading::Symbol<FFIFunction>
             = match lib.get(name.as_bytes()) {
                 Ok(func) => func,
-                Err(e) => return Err(format!("Failed to load function: {}", e)),
+                Err(e) => return Err(format!("failed to load function: {}", e)),
             };
 
-        let mut args_ptr: Vec<*mut std::ffi::c_char> = Vec::new();
+        let result = func(ffi_args.len() as i32, ffi_args.as_ptr());
 
-        args.remove(0);
-        args.remove(0);
-
-        for arg in args {
-            match eval(arg, context) {
-                Ok(AST::Integer(_)) => {
-                    //args_ptr.push(v as *mut std::ffi::c_void);
-                    return Err("Cant use numbers in ffi, it was extremely broken, to be fixed\nSuggestion: turn int to str with str(int), then parse that to int in the lib".to_string());
-                }
-
-                Ok(AST::String(v)) => {
-                    let c_str = std::ffi::CString::new(v.replace("\"", "")).unwrap();
-                    
-                    args_ptr.push(c_str.into_raw() as *mut std::ffi::c_char);
-                }
-
-                Ok(_) => return Err("ffi.call arguments must be numbers or strings".to_string()),
-
-                Err(e) => return Err(e),
-            };
+        for ptr in owned_strings {
+            modu_ffi::ffi_free_string(ptr);
         }
 
-        let result_ptr = func(
-            args_ptr.len() as std::ffi::c_int,
-            args_ptr.as_mut_ptr() as *mut std::ffi::c_char
-        );
+        match result.ty {
+            FFIType::Null => Ok((AST::Null, AST::Null)),
+            FFIType::String => {
+                let c_str = std::ffi::CStr::from_ptr(result.value.string);
+                let str_slice = c_str.to_str().unwrap();
+                let string = str_slice.to_string();
 
-        // lib go bye (i think this prevents memory leaks or something)
-        lib.close().unwrap();
+                modu_ffi::ffi_free_string(result.value.string);
 
-        if result_ptr.is_null() {
-            return Ok((AST::Null, AST::Null));
-        };
-
-        if (result_ptr as i64) <= i32::MAX as i64 && (result_ptr as i64) >= i32::MIN as i64 {
-            return Ok((AST::Integer(result_ptr as i64), AST::Null));
-        } else {
-            let str = std::ffi::CStr::from_ptr(result_ptr as *const _);
-            return Ok((AST::String(str.to_string_lossy().into_owned()), AST::Null))
+                Ok((AST::String(string), AST::Null))
+            }
+            FFIType::Integer => Ok((AST::Integer(result.value.integer), AST::Null)),
+            FFIType::Float => Ok((AST::Float(result.value.float), AST::Null)),
+            FFIType::Boolean => Ok((AST::Boolean(result.value.boolean), AST::Null)),
         }
     }
 }
