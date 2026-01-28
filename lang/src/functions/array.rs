9e77b4a582dd26489b3ac5181215006ec63e1c4b
cyteon <me@cyteon.dev>
2026-01-28 18:12:09 +0100

    add array.pop

--- lang/src/functions/array.rs
+++ lang/src/functions/array.rs
@@ -22,6 +22,24 @@ pub fn push(
     Ok(new_elements)
 }
 
+pub fn pop(
+    elements: &Vec<AST>,
+    args: Vec<AST>,
+) -> Result<(AST, Vec<AST>), String> {
+    if args.len() != 0 {
+        return Err("pop() takes no arguments".to_string());
+    }
+
+    if elements.is_empty() {
+        return Err("Cannot pop from an empty array".to_string());
+    }
+
+    let mut new_elements = elements.clone();
+    let popped_element = new_elements.pop().unwrap();
+
+    Ok((popped_element, new_elements))
+}
+
 pub fn handle_function(
     elements: &Vec<AST>,
     function: String,
@@ -36,6 +54,11 @@ pub fn handle_function(
         "push" => {
             let result = push(elements, args)?;
             Ok((AST::Null, AST::Array(result)))
+        }
+
+        "pop" => {
+            let (popped_element, result) = pop(elements, args)?;
+            Ok((popped_element, AST::Array(result)))
         }
         
         _ => Err(format!("Function '{}' not found in array functions", function)),
