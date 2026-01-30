f379fa68664de833a71510163ba245e4e76c187e
cyteon <me@cyteon.dev>
2026-01-30 19:59:52 +0100

    finish http

--- lang/src/libraries/http.rs
+++ lang/src/libraries/http.rs
@@ -69,6 +69,113 @@ pub fn get(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (String
     handle_response(response)
 }
 
+pub fn post(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (String, Span)> {
+    let url = match &args[0].node {
+        Expr::String(s) => s,
+        _ => return Err((
+            "post expects a string as the first argument".to_string(),
+            args[0].span,
+        )),
+    };
+
+    let body = match &args[1].node {
+        Expr::String(s) => s,
+        _ => return Err((
+            "post expects a string as the second argument".to_string(),
+            args[1].span,
+        )),
+    };
+
+    let client = reqwest::blocking::Client::new();
+    let response = client.post(url)
+        .body(body.clone())
+        .send()
+        .map_err(|e| (
+            format!("Failed to perform POST request: {}", e),
+            args[0].span,
+        ))?;
+
+    handle_response(response)
+}
+
+pub fn put(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (String, Span)> {
+    let url = match &args[0].node {
+        Expr::String(s) => s,
+        _ => return Err((
+            "put expects a string as the first argument".to_string(),
+            args[0].span,
+        )),
+    };
+
+    let body = match &args[1].node {
+        Expr::String(s) => s,
+        _ => return Err((
+            "put expects a string as the second argument".to_string(),
+            args[1].span,
+        )),
+    };
+
+    let client = reqwest::blocking::Client::new();
+    let response = client.put(url)
+        .body(body.clone())
+        .send()
+        .map_err(|e| (
+            format!("Failed to perform PUT request: {}", e),
+            args[0].span,
+        ))?;
+
+    handle_response(response)
+}
+
+pub fn patch(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (String, Span)> {
+    let url = match &args[0].node {
+        Expr::String(s) => s,
+        _ => return Err((
+            "patch expects a string as the first argument".to_string(),
+            args[0].span,
+        )),
+    };
+
+    let body = match &args[1].node {
+        Expr::String(s) => s,
+        _ => return Err((
+            "patch expects a string as the second argument".to_string(),
+            args[1].span,
+        )),
+    };
+
+    let client = reqwest::blocking::Client::new();
+    let response = client.patch(url)
+        .body(body.clone())
+        .send()
+        .map_err(|e| (
+            format!("Failed to perform PATCH request: {}", e),
+            args[0].span,
+        ))?;
+
+    handle_response(response)
+}
+
+pub fn delete(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (String, Span)> {
+    let url = match &args[0].node {
+        Expr::String(s) => s,
+        _ => return Err((
+            "delete expects a string as the first argument".to_string(),
+            args[0].span,
+        )),
+    };
+
+    let client = reqwest::blocking::Client::new();
+    let response = client.delete(url)
+        .send()
+        .map_err(|e| (
+            format!("Failed to perform DELETE request: {}", e),
+            args[0].span,
+        ))?;
+
+    handle_response(response)
+}
+
 pub fn get_object() -> Expr {
     let mut symbols = std::collections::HashMap::new();
 
@@ -79,6 +186,54 @@ pub fn get_object() -> Expr {
                 name: "get".to_string(),
                 args: vec!["url".to_string()],
                 func: get,
+            },
+            span: Span::default(),
+        },
+    );
+
+    symbols.insert(
+        "post".to_string(),
+        SpannedExpr {
+            node: Expr::InternalFunction {
+                name: "post".to_string(),
+                args: vec!["url".to_string(), "body".to_string()],
+                func: post,
+            },
+            span: Span::default(),
+        },
+    );
+
+    symbols.insert(
+        "put".to_string(),
+        SpannedExpr {
+            node: Expr::InternalFunction {
+                name: "put".to_string(),
+                args: vec!["url".to_string(), "body".to_string()],
+                func: put,
+            },
+            span: Span::default(),
+        },
+    );
+
+    symbols.insert(
+        "patch".to_string(),
+        SpannedExpr {
+            node: Expr::InternalFunction {
+                name: "patch".to_string(),
+                args: vec!["url".to_string(), "body".to_string()],
+                func: patch,
+            },
+            span: Span::default(),
+        },
+    );
+
+    symbols.insert(
+        "delete".to_string(),
+        SpannedExpr {
+            node: Expr::InternalFunction {
+                name: "delete".to_string(),
+                args: vec!["url".to_string()],
+                func: delete,
             },
             span: Span::default(),
         },
