045deb8aec79bbf2c7100af168be843f73dd5185
cyteon <me@cyteon.dev>
2026-01-31 12:55:25 +0100

    make an ffi export wrapper

--- ffi/rust_ffi/src/lib.rs
+++ ffi/rust_ffi/src/lib.rs
@@ -1,4 +1,5 @@
-use std::ffi::{c_char, c_double};
+use std::ffi::{c_char, c_double, c_int};
+use paste::paste;
 
 #[repr(C)]
 #[derive(Copy, Clone)]
@@ -13,7 +14,7 @@ pub enum FFIType {
 #[repr(C)]
 pub union FFIValueUnion {
     pub string: *mut c_char,
-    pub integer: i64,
+    pub integer: c_int,
     pub float: c_double,
     pub boolean: bool,
 }
@@ -42,7 +43,7 @@ impl FFIValue {
     pub fn integer(val: i64) -> Self {
         FFIValue {
             ty: FFIType::Integer,
-            value: FFIValueUnion { integer: val },
+            value: FFIValueUnion { integer: val as c_int },
         }
     }
 
@@ -68,4 +69,45 @@ pub extern "C" fn ffi_free_string(ptr: *mut c_char) {
             drop(std::ffi::CString::from_raw(ptr));
         }
     }
+}
+
+#[macro_export]
+macro_rules! ffi_export {
+    ($func_name:ident ( $($arg_name:ident : $arg_type:ty),* ) -> $ret_type:ty $body:block) => {
+        pub fn $fn_name( $( $arg_name : $arg_type ),* ) -> $ret $body
+
+        paste! {
+            #[no_mangle]
+            pub unsafe extern "C" fn [<$fn_name _ffi>](argc: i32, argv: *const FFIValue) -> FFIValue {
+                let args = std::slice::from_raw_parts(argv, argc as usize);
+                let mut i = 0;
+                
+                #(
+                    let $arg_name: $arg_type = match args[i].ty {
+                        FFIType::String => {
+                            let c_str = unsafe { std::ffi::CStr::from_ptr(args[i].value.string) };
+                            let str_slice = c_str.to_str().unwrap();
+                            str_slice.to_string()
+                        },
+                        FFIType::Integer => unsafe { args[i].value.integer },
+                        FFIType::Float => unsafe { args[i].value.float },
+                        FFIType::Boolean => unsafe { args[i].value.boolean },
+                        FFIType::Null => panic!("Unexpected null argument"),
+                    };
+
+                    i += 1;
+                )*
+
+                let result = $func_name( $( $arg_name ),* );
+
+                match result {
+                    FFIValue::String(s) => FFIValue::string(s),
+                    FFIValue::Integer(i) => FFIValue::integer(i),
+                    FFIValue::Float(f) => FFIValue::float(f),
+                    FFIValue::Boolean(b) => FFIValue::boolean(b),
+                    FFIValue::Null => FFIValue::null(),
+                }
+            }
+        }
+    };
 }
