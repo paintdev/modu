3bd2d200d0b509308b7d4d4d9ddb65fe38e1ad9b
cyteon <me@cyteon.dev>
2026-01-31 11:39:40 +0100

    finish array functions

--- lang/src/builtins/array.rs
+++ lang/src/builtins/array.rs
@@ -14,15 +14,77 @@ pub fn len(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (String
     })
 }
 
+pub fn clear(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (String, Span)> {
+    let array = match &args[0].node {
+        Expr::Array(_) => (),
+        _ => unreachable!(),
+    };
+
+    Ok(InternalFunctionResponse {
+        return_value: Expr::Null,
+        replace_self: Some(Expr::Array(vec![])),
+    })
+}
+
+pub fn push(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (String, Span)> {
+    let array = match &args[0].node {
+        Expr::Array(elements) => elements,
+        _ => unreachable!(),
+    };
+
+    if args.len() < 2 {
+        return Err((
+            "push expects 2 arguments".to_string(),
+            args[0].span.clone(),
+        ));
+    }
+
+    let mut new_array = array.clone();
+    new_array.push(args[1].clone());
+
+    Ok(InternalFunctionResponse {
+        return_value: Expr::Null,
+        replace_self: Some(Expr::Array(new_array)),
+    })
+}
+
+pub fn pop(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (String, Span)> {
+    let array = match &args[0].node {
+        Expr::Array(elements) => elements,
+        _ => unreachable!(),
+    };
+
+    if array.is_empty() {
+        return Err((
+            "Cannot pop from an empty array".to_string(),
+            args[0].span.clone(),
+        ));
+    }
+
+    let mut new_array = array.clone();
+    let popped_element = new_array.pop().unwrap();
+
+    Ok(InternalFunctionResponse {
+        return_value: popped_element.node,
+        replace_self: Some(Expr::Array(new_array)),
+    })
+}
+
 pub fn get_fn(name: &str) -> Option<Expr> {
     Some(Expr::InternalFunction {
         name: name.to_string(),
         args: match name {
             "len" => vec!["self".to_string()],
+            "clear" => vec!["self".to_string()],
+            "push" => vec!["self".to_string(), "value".to_string()],
+            "pop" => vec!["self".to_string()],
             _ => vec![],
         },
         func: match name {
             "len" => len,
+            "clear" => clear,
+            "push" => push,
+            "pop" => pop,
             _ => return None,
         },
     })
